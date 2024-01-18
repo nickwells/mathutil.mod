d8a1ff51b062077f5bdefcf76c17bd6065d74868
Nick Wells <wells.nicholas@gmail.com>
2024-01-18 15:02:03 +0000

    feature: add the RationalApproximation funcs

---
+++ mathutil/ratApprox.go
@@ -0,0 +1,222 @@
+package mathutil
+
+import (
+	"errors"
+	"math"
+)
+
+// MaxFareyTrials is the maximum number of attempts that will be made to find
+// a Rational Approximation by taking entries in the Farey Sequence. If no
+// approximation within the accuracy threshold has been found by this point
+// an error is returned indicating that the Farey sequence converges too
+// slowly. Conversion can be very slow for values, for instance, with
+// fractional parts very close to the ends of the interval [0,1].
+const MaxFareyTrials = 100
+
+const raErrSuffix = ", no rational approximation is possible"
+
+var (
+	errInaccurate  = errors.New("couldn't meet accuracy target" + raErrSuffix)
+	errTooBig      = errors.New("the value is too big" + raErrSuffix)
+	errIsInf       = errors.New("the value is infinite" + raErrSuffix)
+	errIsNaN       = errors.New("the value is not a number" + raErrSuffix)
+	errBadAccuracy = errors.New("accuracy must be >0 and <100" + raErrSuffix)
+	errConvTooSlow = errors.New("Farey seq converges too slowly" + raErrSuffix)
+)
+
+// RationalApproximationByFareysAlgo returns a Rational approximation for v.
+// Various float64 values cannot be represented in this way (including values
+// that are too big) and a non-nil error is returned in this case. It uses
+// Farey's algorithm to generate a sequence of rational approximations.
+//
+// The accuracy must be less than 100 and greater than zero.
+//
+// This may generate different approximations from the RationalApproximation
+// func.
+//
+// Note that this will try at most MaxFareyTrials times before giving up. It
+// can be very slow to converge to certain values, particularly those close
+// to zero or one. If an error is returned the
+func RationalApproximationByFareysAlgo(v, accuracy float64) (Rational, error) {
+	var r Rational
+
+	if err := checkRationalApproxParams(v, accuracy); err != nil {
+		return r, err
+	}
+	accuracy /= 100.0
+
+	vAbs, sign := normaliseRationalApproxVal(v)
+
+	intPart := math.Floor(vAbs)
+	fracPart := vAbs - intPart
+	if fracPart == 0 {
+		return Rational{int64(intPart) * sign, 1}, nil
+	}
+
+	var (
+		lower = Rational{N: 0, D: 1}
+		upper = Rational{N: 1, D: 1}
+	)
+
+	for i := 0; i < MaxFareyTrials; i++ {
+		mediant, err := mediant(lower, upper)
+		if err != nil {
+			return r, errors.New(err.Error() + raErrSuffix)
+		}
+		r, err = SetRational(intPart, mediant.N, mediant.D, sign)
+		if err != nil {
+			return r, err
+		}
+
+		if r.Proximity(v) <= accuracy {
+			return r, nil
+		}
+
+		rv := mediant.AsFloat64()
+		if fracPart > rv {
+			lower = mediant
+		} else {
+			upper = mediant
+		}
+	}
+
+	return r, errConvTooSlow
+}
+
+// normaliseRationalApproxVal converts v to its absolute value and returns it
+// together with a sign value
+func normaliseRationalApproxVal(v float64) (float64, int64) {
+	vAbs := math.Abs(v)
+	var sign int64 = 1
+	if math.Signbit(v) {
+		sign = -1
+	}
+
+	return vAbs, sign
+}
+
+// continuedFraction returns a slice containing at most maxVals entries which
+// represents the first entries in the continued fractions representation of
+// v.
+func continuedFraction(v float64, maxVals uint) ([]int64, error) {
+	if math.IsInf(v, 1) ||
+		math.IsInf(v, -1) {
+		return nil, errors.New("the value is infinite")
+	}
+	if math.IsNaN(v) {
+		return nil, errors.New("the value is not a number")
+	}
+
+	cf := make([]int64, 0, maxVals)
+
+	for i := uint(0); i < maxVals; i++ {
+		intPart := math.Floor(v)
+		if intPart > float64(math.MaxInt64) ||
+			float64(int64(intPart)) != intPart {
+			return cf, errors.New("continued fraction value too big")
+		}
+		cf = append(cf, int64(intPart))
+		v -= intPart
+		if v <= 0 {
+			break
+		}
+		v = 1 / v
+	}
+
+	return cf, nil
+}
+
+// checkRationalAccuracy returns a non nil error if accuracy <= 0 or >= 100
+func checkRationalAccuracy(accuracy float64) error {
+	if accuracy <= 0.0 || accuracy >= 100.0 {
+		return errBadAccuracy
+	}
+	return nil
+}
+
+// checkRationalTargetVal returns a non-nil error if the value is not a
+// regular float64 value
+func checkRationalTargetVal(v float64) error {
+	if math.IsInf(v, 1) ||
+		math.IsInf(v, -1) {
+		return errIsInf
+	}
+	if math.IsNaN(v) {
+		return errIsNaN
+	}
+	if v >= float64(math.MaxInt64) ||
+		v < float64(math.MinInt64) {
+		return errTooBig
+	}
+	return nil
+}
+
+// checkRationalApproxParams checks that the parameters to
+// RationalApproximation are valid and returns an error if not
+func checkRationalApproxParams(v float64, accuracy float64) error {
+	if err := checkRationalAccuracy(accuracy); err != nil {
+		return err
+	}
+
+	if err := checkRationalTargetVal(v); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RationalApproximation returns a Rational value (a numerator N and
+// denominator D) and an error. The values are such that the supplied value v
+// will lie within accuracy percent of N/D. It uses continued fractions to
+// generate the rational coefficients. Various float64 values cannot be
+// represented in this way (including values that are too big) and a non-nil
+// error is returned in this case.
+//
+// The accuracy must be less than 100 and greater than zero.
+//
+// This may generate different approximations from the
+// RationalApproximationByFareysAlgo func.
+func RationalApproximation(v, accuracy float64) (Rational, error) {
+	var r Rational
+	if v == 0 {
+		return Rational{N: 0, D: 1}, nil
+	}
+
+	if err := checkRationalApproxParams(v, accuracy); err != nil {
+		return r, err
+	}
+	accuracy /= 100.0
+
+	vAbs, sign := normaliseRationalApproxVal(v)
+
+	const maxCFLen = 20
+
+	for cfLen := uint(1); cfLen <= maxCFLen; cfLen++ {
+		cf, err := continuedFraction(vAbs, cfLen)
+		if err != nil && len(cf) == 0 {
+			return r, errors.New(err.Error() + raErrSuffix)
+		}
+
+		r = Rational{N: 1, D: cf[len(cf)-1]}
+		for i := len(cf) - 2; i >= 0; i-- {
+			if math.MaxInt64/r.D < cf[i] { // restart, ignoring the end values
+				r = Rational{N: 1, D: cf[i]}
+				continue
+			}
+			p := cf[i] * r.D
+			if math.MaxInt64-p < r.N { // restart, ignoring the end values
+				r = Rational{N: 1, D: cf[i]}
+				continue
+			}
+			r.N += p
+			r = r.Invert()
+		}
+		r = r.Invert() // undo the last swap
+
+		r.N *= sign
+		if r.Proximity(v) <= accuracy {
+			return r, nil
+		}
+	}
+	return r, errInaccurate
+}
---
+++ mathutil/ratApprox_test.go
@@ -0,0 +1,369 @@
+package mathutil
+
+import (
+	"math"
+	"testing"
+
+	"github.com/nickwells/testhelper.mod/v2/testhelper"
+)
+
+func TestRationalApproximation(t *testing.T) {
+	testCases := []struct {
+		testhelper.ID
+		v        float64
+		accuracy float64
+		expRat   Rational
+		testhelper.ExpErr
+	}{
+		{
+			ID:       testhelper.MkID("zero"),
+			v:        0,
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 1},
+		},
+		{
+			ID:       testhelper.MkID("one"),
+			v:        1,
+			accuracy: 1,
+			expRat:   Rational{N: 1, D: 1},
+		},
+		{
+			ID:       testhelper.MkID("minus one"),
+			v:        -1,
+			accuracy: 1,
+			expRat:   Rational{N: -1, D: 1},
+		},
+		{
+			ID:       testhelper.MkID("0.65 to 10%"),
+			v:        0.65,
+			accuracy: 1e1,
+			expRat:   Rational{N: 2, D: 3},
+		},
+		{
+			ID:       testhelper.MkID("0.65 to 0.1%"),
+			v:        0.65,
+			accuracy: 1e-1,
+			expRat:   Rational{N: 13, D: 20},
+		},
+		{
+			ID:       testhelper.MkID("0.65, very accurate"),
+			v:        0.65,
+			accuracy: 1e-20,
+			expRat:   Rational{N: 13, D: 20},
+		},
+		{
+			ID:       testhelper.MkID("small value, very accurate"),
+			v:        1.23e-10,
+			accuracy: 1e-20,
+			expRat:   Rational{N: 123, D: 1000000000000},
+		},
+		{
+			ID:       testhelper.MkID("very small value, very accurate"),
+			v:        1.23e-20,
+			accuracy: 1e-20,
+			expRat:   Rational{N: 0, D: 1},
+			ExpErr:   testhelper.MkExpErr(errInaccurate.Error()),
+		},
+		{
+			ID:       testhelper.MkID("Pi to 1%"),
+			v:        math.Pi,
+			accuracy: 1,
+			expRat:   Rational{N: 22, D: 7},
+		},
+		{
+			ID:       testhelper.MkID("Pi to 0.001%"),
+			v:        math.Pi,
+			accuracy: 0.001,
+			expRat:   Rational{N: 355, D: 113},
+		},
+		{
+			ID:       testhelper.MkID("Pi to maximum accuracy"),
+			v:        math.Pi,
+			accuracy: math.SmallestNonzeroFloat64,
+			expRat:   Rational{N: 245850922, D: 78256779},
+		},
+		{
+			ID:       testhelper.MkID("MaxInt64"),
+			v:        math.MaxInt64,
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errTooBig.Error()),
+		},
+		{
+			ID:       testhelper.MkID("+ve infinity"),
+			v:        math.Inf(1),
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errIsInf.Error()),
+		},
+		{
+			ID:       testhelper.MkID("-ve infinity"),
+			v:        math.Inf(-1),
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errIsInf.Error()),
+		},
+		{
+			ID:       testhelper.MkID("NaN"),
+			v:        math.NaN(),
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errIsNaN.Error()),
+		},
+		{
+			ID:       testhelper.MkID("bad accuracy - negative"),
+			v:        1,
+			accuracy: -1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errBadAccuracy.Error()),
+		},
+		{
+			ID:       testhelper.MkID("bad accuracy - zero"),
+			v:        1,
+			accuracy: 0,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errBadAccuracy.Error()),
+		},
+		{
+			ID:       testhelper.MkID("bad accuracy - 100"),
+			v:        1,
+			accuracy: 100,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errBadAccuracy.Error()),
+		},
+		{
+			ID:       testhelper.MkID("bad accuracy - too big"),
+			v:        1,
+			accuracy: 101,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errBadAccuracy.Error()),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.ID.Name, func(t *testing.T) {
+			r, err := RationalApproximation(tc.v, tc.accuracy)
+			testhelper.DiffInt[int64](t, tc.IDStr(),
+				"Numerator", r.N, tc.expRat.N)
+			testhelper.DiffInt[int64](t, tc.IDStr(),
+				"Denominator", r.D, tc.expRat.D)
+			testhelper.CheckExpErr(t, err, tc)
+		})
+	}
+}
+
+func TestRationalApproximationByFareysAlgo(t *testing.T) {
+	testCases := []struct {
+		testhelper.ID
+		v        float64
+		accuracy float64
+		expRat   Rational
+		testhelper.ExpErr
+	}{
+		{
+			ID:       testhelper.MkID("zero"),
+			v:        0,
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 1},
+		},
+		{
+			ID:       testhelper.MkID("one"),
+			v:        1,
+			accuracy: 1,
+			expRat:   Rational{N: 1, D: 1},
+		},
+		{
+			ID:       testhelper.MkID("minus one"),
+			v:        -1,
+			accuracy: 1,
+			expRat:   Rational{N: -1, D: 1},
+		},
+		{
+			ID:       testhelper.MkID("0.65 to 10%"),
+			v:        0.65,
+			accuracy: 1e1,
+			expRat:   Rational{N: 2, D: 3},
+		},
+		{
+			ID:       testhelper.MkID("0.65 to 0.1%"),
+			v:        0.65,
+			accuracy: 1e-1,
+			expRat:   Rational{N: 13, D: 20},
+		},
+		{
+			ID:       testhelper.MkID("0.65, very accurate"),
+			v:        0.65,
+			accuracy: 1e-20,
+			expRat:   Rational{N: 13, D: 20},
+		},
+		{
+			ID:       testhelper.MkID("small value, very accurate"),
+			v:        1.23e-10,
+			accuracy: 1e-20,
+			expRat:   Rational{N: 1, D: 101},
+			ExpErr:   testhelper.MkExpErr(errConvTooSlow.Error()),
+		},
+		{
+			ID:       testhelper.MkID("very small value, very accurate"),
+			v:        1.23e-20,
+			accuracy: 1e-20,
+			expRat:   Rational{N: 1, D: 101},
+			ExpErr:   testhelper.MkExpErr(errConvTooSlow.Error()),
+		},
+		{
+			ID:       testhelper.MkID("Pi to 1%"),
+			v:        math.Pi,
+			accuracy: 1,
+			expRat:   Rational{N: 19, D: 6},
+		},
+		{
+			ID:       testhelper.MkID("Pi to 0.001%"),
+			v:        math.Pi,
+			accuracy: 0.001,
+			expRat:   Rational{N: 355, D: 113},
+		},
+		{
+			ID:       testhelper.MkID("Pi to maximum accuracy"),
+			v:        math.Pi,
+			accuracy: math.SmallestNonzeroFloat64,
+			expRat:   Rational{N: 28023, D: 8920},
+			ExpErr:   testhelper.MkExpErr(errConvTooSlow.Error()),
+		},
+		{
+			ID:       testhelper.MkID("MaxInt64"),
+			v:        math.MaxInt64,
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errTooBig.Error()),
+		},
+		{
+			ID:       testhelper.MkID("+ve infinity"),
+			v:        math.Inf(1),
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errIsInf.Error()),
+		},
+		{
+			ID:       testhelper.MkID("-ve infinity"),
+			v:        math.Inf(-1),
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errIsInf.Error()),
+		},
+		{
+			ID:       testhelper.MkID("NaN"),
+			v:        math.NaN(),
+			accuracy: 1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errIsNaN.Error()),
+		},
+		{
+			ID:       testhelper.MkID("bad accuracy - negative"),
+			v:        1,
+			accuracy: -1,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errBadAccuracy.Error()),
+		},
+		{
+			ID:       testhelper.MkID("bad accuracy - zero"),
+			v:        1,
+			accuracy: 0,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errBadAccuracy.Error()),
+		},
+		{
+			ID:       testhelper.MkID("bad accuracy - 100"),
+			v:        1,
+			accuracy: 100,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errBadAccuracy.Error()),
+		},
+		{
+			ID:       testhelper.MkID("bad accuracy - too big"),
+			v:        1,
+			accuracy: 101,
+			expRat:   Rational{N: 0, D: 0},
+			ExpErr:   testhelper.MkExpErr(errBadAccuracy.Error()),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.ID.Name, func(t *testing.T) {
+			r, err := RationalApproximationByFareysAlgo(tc.v, tc.accuracy)
+			testhelper.DiffInt[int64](t, tc.IDStr(),
+				"Numerator", r.N, tc.expRat.N)
+			testhelper.DiffInt[int64](t, tc.IDStr(),
+				"Denominator", r.D, tc.expRat.D)
+			testhelper.CheckExpErr(t, err, tc)
+		})
+	}
+}
+
+func TestContinuedFraction(t *testing.T) {
+	const errText = "continued fraction value too big"
+	testCases := []struct {
+		testhelper.ID
+		v       float64
+		maxVals uint
+		expVals []int64
+		testhelper.ExpErr
+	}{
+		{
+			ID:      testhelper.MkID("Pi, 5 vals"),
+			v:       math.Pi,
+			maxVals: 5,
+			expVals: []int64{3, 7, 15, 1, 292},
+		},
+		{
+			ID:      testhelper.MkID("0.65, 13 vals"),
+			v:       0.65,
+			maxVals: 13,
+			expVals: []int64{
+				0, 1, 1, 1, 6,
+				46912496118442, 1, 1, 1, 42,
+				6701785159777, 1, 1,
+			},
+		},
+		{
+			ID:      testhelper.MkID("big val, 2 vals"),
+			v:       2 * math.MaxInt64,
+			maxVals: 2,
+			expVals: []int64{},
+			ExpErr:  testhelper.MkExpErr(errText),
+		},
+		{
+			ID:      testhelper.MkID("small fraction>1, 2 vals"),
+			v:       5 + 1e-18,
+			maxVals: 2,
+			expVals: []int64{5},
+		},
+		{
+			ID:      testhelper.MkID("very small fraction>1, 2 vals"),
+			v:       5 + 1e-19,
+			maxVals: 2,
+			expVals: []int64{5},
+		},
+		{
+			ID:      testhelper.MkID("very small fraction<1, 2 vals"),
+			v:       1e-19,
+			maxVals: 2,
+			expVals: []int64{0},
+			ExpErr:  testhelper.MkExpErr(errText),
+		},
+		{
+			ID:      testhelper.MkID("very small fraction, 200 vals"),
+			v:       1e-100,
+			maxVals: 200,
+			expVals: []int64{0},
+			ExpErr:  testhelper.MkExpErr(errText),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.ID.Name, func(t *testing.T) {
+			vals, err := continuedFraction(tc.v, tc.maxVals)
+			testhelper.DiffSlice[int64](t, tc.IDStr(), "vals", vals, tc.expVals)
+			testhelper.CheckExpErr(t, err, tc)
+		})
+	}
+}
