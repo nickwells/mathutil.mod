f746bfbd53f89b4a63b824175af4f009647ecd98
Nick Wells <wells.nicholas@gmail.com>
2024-01-18 14:49:46 +0000

    feature: add the Rational type

---
+++ mathutil/rational.go
@@ -0,0 +1,72 @@
+package mathutil
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+var (
+	errNumerTooBig = errors.New("overflow: the numerator is too big")
+	errDenomTooBig = errors.New("overflow: the denominator is too big")
+)
+
+// Rational represents a rational number. It is used as the return value of
+// the RationalApproximation functions.
+type Rational struct {
+	N int64
+	D int64
+}
+
+// String returns a string value for the Rational
+func (r Rational) String() string {
+	return fmt.Sprintf("Rational{N: %19d, D: %19d}", r.N, r.D)
+}
+
+// AsFloat64 returns the float64 equivalent of the Rational
+func (r Rational) AsFloat64() float64 {
+	return float64(r.N) / float64(r.D)
+}
+
+// Invert returns 1/r
+func (r Rational) Invert() Rational {
+	return Rational{N: r.D, D: r.N}
+}
+
+// Proximity returns the absolute difference between the rational value and
+// the supplied value as a proportion of the supplied value
+func (r Rational) Proximity(v float64) float64 {
+	return math.Abs((r.AsFloat64() - v) / v)
+}
+
+// mediant generates the Mediant of the two values; given a/b and c/d the
+// Mediant is a+c/b+d. If the lower and upper values satisfy the properties
+// that a/b < c/d and bc-ad == 1 then the Mediant is guaranteed to lie
+// between them. Additionally the new value also satisfies this property with
+// respect to lower and upper values respectively. That is, if we take a/b
+// and c/d such that a/b < c/d and bc-ad == 1 then p/q where p=a+c and q=b+d
+// will also have the property that bp-aq == 1 and qc-pd == 1.
+//
+// It will return a non-nil error if any of the sums overflow. Note that it
+// does not check that the Mediant property holds, this is the responsibility
+// of the caller.
+func mediant(lower, upper Rational) (Rational, error) {
+	if math.MaxInt64-lower.N < upper.N {
+		return Rational{0, 1}, errNumerTooBig
+	}
+	if math.MaxInt64-lower.D < upper.D {
+		return Rational{0, 1}, errDenomTooBig
+	}
+	return Rational{N: lower.N + upper.N, D: lower.D + upper.D}, nil
+}
+
+// SetRational constructs a Rational from the passed values, checks for
+// overflows and returns it along with any error
+func SetRational(intPart float64, n, d, sign int64) (Rational, error) {
+	numerator := float64(d)*intPart + float64(n)
+	if numerator > float64(math.MaxInt64) {
+		return Rational{}, errTooBig
+	}
+
+	return Rational{N: (d*int64(intPart) + n) * sign, D: d}, nil
+}
---
+++ mathutil/rational_test.go
@@ -0,0 +1,51 @@
+package mathutil
+
+import (
+	"math"
+	"testing"
+
+	"github.com/nickwells/testhelper.mod/v2/testhelper"
+)
+
+func TestMediant(t *testing.T) {
+	const bigVal = 3 * math.MaxInt64 / 4
+	testCases := []struct {
+		testhelper.ID
+		lower  Rational
+		upper  Rational
+		expMed Rational
+		testhelper.ExpErr
+	}{
+		{
+			ID:     testhelper.MkID("0-1, 1/2"),
+			lower:  Rational{0, 1},
+			upper:  Rational{1, 1},
+			expMed: Rational{1, 2},
+		},
+		{
+			ID:     testhelper.MkID("numerator overflow"),
+			lower:  Rational{bigVal, bigVal + 1},
+			upper:  Rational{bigVal + 1, bigVal + 2},
+			expMed: Rational{0, 1},
+			ExpErr: testhelper.MkExpErr(errNumerTooBig.Error()),
+		},
+		{
+			ID:     testhelper.MkID("denominator overflow"),
+			lower:  Rational{1, bigVal + 2},
+			upper:  Rational{1, bigVal + 1},
+			expMed: Rational{0, 1},
+			ExpErr: testhelper.MkExpErr(errDenomTooBig.Error()),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.ID.Name, func(t *testing.T) {
+			m, err := mediant(tc.lower, tc.upper)
+			testhelper.DiffInt[int64](t, tc.IDStr(), "Numerator",
+				m.N, tc.expMed.N)
+			testhelper.DiffInt[int64](t, tc.IDStr(), "Denominator",
+				m.D, tc.expMed.D)
+			testhelper.CheckExpErr(t, err, tc)
+		})
+	}
+}
